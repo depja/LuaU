db96d01592ab19de10de4587f1dce60551b79782
dep <103852505+depja@users.noreply.github.com>
2025-11-22 11:08:29 +0000

    Update Linoria.lua

--- Linoria.lua
+++ Linoria.lua
@@ -42,7 +42,7 @@ local Library = {
 	FontColor = Color3.fromRGB(255, 255, 255);
 	MainColor = Color3.fromRGB(0, 0, 0);
 	BackgroundColor = Color3.fromRGB(0, 0, 0);
-	AccentColor = Color3.fromRGB(0, 85, 255);
+	AccentColor = Color3.fromRGB(255, 175, 255);
 	OutlineColor = Color3.fromRGB(50, 50, 50);
 	RiskColor = Color3.fromRGB(255, 50, 50),
 
